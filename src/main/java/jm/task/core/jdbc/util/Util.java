e21d7a7d32caee1ebcfd1e9c4f2c8ba04366df5e
wendelca <vvkamalov_obs@mail.ru>
2024-08-29 13:21:20 +0500

    add application.properties file for work with db credentials

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,18 +7,21 @@ import java.util.Optional;
 
 public class Util {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/first_project_schema";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
 
     private Util() {
-        throw new UnsupportedOperationException("У класса не может быть экземляра");
+        throw new UnsupportedOperationException("У данного класса не может быть экземляра");
     }
 
     public static Optional<Connection> getDBConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return Optional.of(DriverManager.getConnection(URL, USERNAME, PASSWORD));
+            return Optional.of(DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)));
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Ошибка загрузки драйвера базы данных", e);
         } catch (SQLException e) {
