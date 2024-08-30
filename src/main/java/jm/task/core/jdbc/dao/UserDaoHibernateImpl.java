df15632e2b2e2e2957bcbddb89fe9ad06877bfc0
wendelca <vvkamalov_obs@mail.ru>
2024-08-30 16:58:09 +0500

    finish of the hibernate part

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -7,172 +7,77 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class UserDaoHibernateImpl implements UserDao {
 
-    private SessionFactory sessionFactory;
+    private static final SessionFactory sessionFactory = Util.getHibernateSessionFactory();
 
     public UserDaoHibernateImpl() {}
 
-    private void initSessionFactory() {
-        this.sessionFactory = Util.getHibernateSessionFactory();
-    }
-
-
     @Override
     public void createUsersTable() {
-        if (sessionFactory == null) {
-            initSessionFactory();
-        }
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            String sqlCreateTableQuery = "CREATE TABLE IF NOT EXISTS users ("
-                    + "user_id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT, "
-                    + "name VARCHAR(50) NOT NULL, "
-                    + "last_name VARCHAR(50) NOT NULL, "
-                    + "age TINYINT UNSIGNED NOT NULL CHECK (age >= 0 AND age <= 127)"
-                    + ");";
-            session.createNativeQuery(sqlCreateTableQuery).executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                try {
-                    transaction.rollback();
-                } catch (Exception rollbackException) {
-                    System.err.println("Ошибка при откате транзакции: "
-                            + rollbackException.getMessage());
-                }
-            }
-            throw new RuntimeException("Ошибка при создании таблицы пользователей", e);
-        }
+        String sqlCreateTableQuery = "CREATE TABLE IF NOT EXISTS users ("
+                + "user_id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT, "
+                + "name VARCHAR(50) NOT NULL, "
+                + "last_name VARCHAR(50) NOT NULL, "
+                + "age TINYINT UNSIGNED NOT NULL CHECK (age >= 0 AND age <= 127)"
+                + ");";
+        executeDDLQuery(sqlCreateTableQuery);
     }
 
     @Override
     public void dropUsersTable() {
-        if (sessionFactory == null) {
-            initSessionFactory();
-        }
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            String sqlDropTableQuery = "DROP TABLE IF EXISTS users;";
-            session.createNativeQuery(sqlDropTableQuery).executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                try {
-                    transaction.rollback();
-                } catch (Exception rollbackException) {
-                    System.err.println("Ошибка при откате транзакции: "
-                            + rollbackException.getMessage());
-                }
-            }
-            throw new RuntimeException("Ошибка при удалении таблицы из базы данных", e);
-        }
+        String sqlDropTableQuery = "DROP TABLE IF EXISTS users;";
+        executeDDLQuery(sqlDropTableQuery);
+    }
+
+    @Override
+    public void cleanUsersTable() {
+        String sqlCleanUsersTableQuery = "TRUNCATE TABLE users;";
+        executeDDLQuery(sqlCleanUsersTableQuery);
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        if (sessionFactory == null) {
-            initSessionFactory();
-        }
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+        executeDMLorDQLQuery(session -> {
             String sqlSaveUserQuery = "INSERT INTO users (name, last_name, age) VALUES (:name, :lastName, :age);";
             session.createNativeQuery(sqlSaveUserQuery)
                     .setParameter("name", name)
                     .setParameter("lastName", lastName)
                     .setParameter("age", age)
                     .executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                try {
-                    transaction.rollback();
-                } catch (Exception rollbackException) {
-                    System.err.println("Ошибка при откате транзакции: "
-                            + rollbackException.getMessage());
-                }
-            }
-            throw new RuntimeException("Ошибка при сохранении пользователя", e);
-        }
+            return null;
+        });
     }
 
     @Override
     public void removeUserById(long id) {
-        if (sessionFactory == null) {
-            initSessionFactory();
-        }
-        Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+        executeDMLorDQLQuery(session -> {
             String sqlRemoveUserQuery = "DELETE FROM users WHERE user_id = :id";
             session.createNativeQuery(sqlRemoveUserQuery)
                     .setParameter("id", id)
                     .executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                try {
-                    transaction.rollback();
-                } catch (Exception rollbackException) {
-                    System.err.println("Ошибка при откате транзакции: "
-                            + rollbackException.getMessage());
-                }
-            }
-            throw new RuntimeException("Ошибка при удалении определенного пользователя из таблиц", e);
-        }
+            return null;
+        });
     }
 
     @Override
     public List<User> getAllUsers() {
-        if (sessionFactory == null) {
-            initSessionFactory();
-        }
-        Transaction transaction = null;
-        List<User> users = new ArrayList<>();
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+        return executeDMLorDQLQuery(session -> {
             String sqlGetAllUsersQuery = "SELECT user_id, name, last_name, age FROM users;";
-            Query<Object[]> query = session.createNativeQuery(sqlGetAllUsersQuery);
-            List<Object[]> results = query.getResultList();
-            for (Object[] result : results) {
-                User user = new User();
-                user.setId(Long.parseLong(result[0].toString()));
-                user.setName(result[1].toString());
-                user.setLastName(result[2].toString());
-                user.setAge(Byte.parseByte(result[3].toString()));
-                users.add(user);
-            }
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                try {
-                    transaction.rollback();
-                } catch (Exception rollbackException) {
-                    System.err.println("Ошибка при откате транзакции: "
-                            + rollbackException.getMessage());
-                }
-            }
-            throw new RuntimeException("Ошибка при получении списка пользователей из таблицы", e);
-        }
-        return users;
+            return session
+                    .createNativeQuery(sqlGetAllUsersQuery, User.class)
+                    .getResultList();
+        });
     }
 
-    @Override
-    public void cleanUsersTable() {
-        if (sessionFactory == null) {
-            initSessionFactory();
-        }
+    private void executeDDLQuery(String sql) {
         Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
-            String sqlCleanUsersTableQuery = "TRUNCATE TABLE users;";
-            session.createNativeQuery(sqlCleanUsersTableQuery).executeUpdate();
+            session.createNativeQuery(sql).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
@@ -183,7 +88,28 @@ public class UserDaoHibernateImpl implements UserDao {
                             + rollbackException.getMessage());
                 }
             }
-            throw new RuntimeException("Ошибка при очистке таблицы пользователей", e);
+            throw new RuntimeException("Ошибка при обработке DDL запроса", e);
         }
+    }
+
+    private <R> R executeDMLorDQLQuery(Function<Session, R> operation) {
+        Transaction transaction = null;
+        R result;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            result = operation.apply(session);
+            transaction.commit();
+        } catch (Exception e) {
+            try {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+            } catch (Exception rollbackException) {
+                System.err.println("Ошибка при откате транзакции: "
+                        + rollbackException.getMessage());
+            }
+            throw new RuntimeException("Ошибка при обработке DML или DQL запроса", e);
+        }
+        return result;
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,43 +6,24 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
 
 public class Util {
 
-    private static final String URL_KEY = "jdbc.url";
-    private static final String USERNAME_KEY = "jdbc.username";
-    private static final String PASSWORD_KEY = "jdbc.password";
-
     private static SessionFactory sessionFactory;
-    private static EntityManagerFactory entityManagerFactory;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/first_project_schema";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     private Util() {
         throw new UnsupportedOperationException("У данного класса не может быть экземляра");
-    }
-
-    public static Optional<Connection> getJDBCConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return Optional.of(DriverManager.getConnection(
-                    JDBCPropertiesUtil.get(URL_KEY),
-                    JDBCPropertiesUtil.get(USERNAME_KEY),
-                    JDBCPropertiesUtil.get(PASSWORD_KEY)));
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Ошибка загрузки драйвера базы данных", e);
-        } catch (SQLException e) {
-            throw new RuntimeException("Ошибка подключения к базе данных", e);
-        }
     }
 
     public static SessionFactory getHibernateSessionFactory() {
@@ -54,9 +35,9 @@ public class Util {
 
                 envVariablesHandle(properties);
 
-                Configuration configuration = new Configuration();
-                configuration.addAnnotatedClass(User.class);
-                configuration.addProperties(properties);
+                Configuration configuration = new Configuration()
+                        .addAnnotatedClass(User.class)
+                        .addProperties(properties);
                 ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
                         .applySettings(configuration.getProperties())
                         .build();
@@ -74,39 +55,27 @@ public class Util {
         String dbUsername = System.getenv("DB_USERNAME");
         String dbPassword = System.getenv("DB_PASSWORD");
         if (dbUsername == null && dbPassword == null) {
-            throw new RuntimeException("Не установлены переменные окружения DB_USERNAME или DB_PASSWORD");
+            throw new RuntimeException("Не установлены переменные окружения - DB_USERNAME и(или) DB_PASSWORD");
         }
         properties.setProperty("hibernate.connection.username", dbUsername);
         properties.setProperty("hibernate.connection.password", dbPassword);
     }
 
-    public static EntityManagerFactory getEntityManager() {
-        if (entityManagerFactory == null) {
-            try (InputStream inputStream = Util.class.getClassLoader()
-                    .getResourceAsStream("hibernate.properties")) {
-                Properties properties = new Properties();
-                properties.load(inputStream);
-
-                String dbUsername = System.getenv("DB_USERNAME");
-                String dbPassword = System.getenv("DB_PASSWORD");
-                if (dbUsername == null && dbPassword == null) {
-                    throw new RuntimeException("Не установлены переменные окружения DB_USERNAME или DB_PASSWORD");
-                }
-                properties.setProperty("hibernate.connection.username", dbUsername);
-                properties.setProperty("hibernate.connection.password", dbPassword);
-
-                Map<String, Object> configOverrides = new HashMap<>();
-                properties.forEach((key, value) -> configOverrides.put(key.toString(), value.toString()));
-
-                entityManagerFactory = Persistence
-                        .createEntityManagerFactory("myPersistenceUnit", configOverrides);
-            } catch (IOException e) {
-                throw new RuntimeException("Ошибка при работе с файлом hibernate.properties", e);
-            } catch (Exception e) {
-                throw new RuntimeException("Ошибка при создании ServiceFactory", e);
-            }
+    public static void sessionFactoryClose() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
         }
-        return entityManagerFactory;
+    }
+
+    public static Optional<Connection> getJDBCConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return Optional.of(DriverManager.getConnection(URL, USERNAME, PASSWORD));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Ошибка загрузки драйвера базы данных", e);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка подключения к базе данных", e);
+        }
     }
 }
 
